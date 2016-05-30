cd7ed36263fa8c0abebea340aba6a9bc7ba4b0fa
tux <mazzu83@gmail.com>
2016-05-30 23:06:20 +0200

    Add contact render

--- personal/urls.py
+++ personal/urls.py
@@ -17,5 +17,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^\Z', views.index, name='index'),  #index page
+    url(r'^$', views.index, name='index'),  #index page
+    url(r'^contact/$', views.contact, name='contact'),  #contact page
+
 ]
--- personal/views.py
+++ personal/views.py
@@ -5,9 +5,13 @@ def index(request):
     return render(request, 'personal/home.html')
 
 
+def test():
+    return 2 * 3
+
+
 def contact(request):
-    return render(request, 'personal/basic.html', {
-                                                    'content:': [
-                                                        'If you want to contact me, please email me', 'email.gmail.com'
-                                                        ]
-                                                    })
+    return render(request, 'personal/basic.html', {'content1': [
+        'If you want to contact me, please email me', 'email.gmail.com',
+    ],
+        'fromFunction': test()
+    })
