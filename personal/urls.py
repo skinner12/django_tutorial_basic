9bf787dff4250017808c2854926aba423c3e78ba
tux <mazzu83@gmail.com>
2016-05-30 22:22:12 +0200

    Add contact render

--- personal/urls.py
+++ personal/urls.py
@@ -17,5 +17,5 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^', views.index, name='index'),  #index page
+    url(r'^\Z', views.index, name='index'),  #index page
 ]
--- personal/views.py
+++ personal/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'personal/home.html')
+
+
+def contact(request):
+    return render(request, 'personal/basic.html', {
+                                                    'content:': [
+                                                        'If you want to contact me, please email me', 'email.gmail.com'
+                                                        ]
+                                                    })
