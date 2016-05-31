2e321e7ab0ca3ec7c3802e92f35d898a7e3261c9
tux <mazzu83@gmail.com>
2016-05-31 17:09:59 +0200

    Make migration

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('personal.urls')),  #index page
+    url(r'^blog/', include('blog.urls')),  #index page
 ]
