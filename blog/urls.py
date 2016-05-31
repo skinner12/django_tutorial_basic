3f42f71ccfe852117751711e1f4330f86c508835
tux <mazzu83@gmail.com>
2016-05-31 17:47:00 +0200

    Added posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from blog.models import Post
 
 urlpatterns = [
     url(r'^$', ListView.as_view(queryset=Post.objects.all().order_by('-date')[:25], template_name='blog/blog.html')),  #index page
+    url(r'^(?P<pk>\d+)$', DetailView.as_view(model=Post,template_name='blog/post.html'))
 
 ]
